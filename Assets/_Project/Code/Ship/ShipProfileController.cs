e3ce37709d682e355dc59d75c6685698a9b2f294
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-09-13 23:31:17 +0300

    feat(ship): add data-driven ship catalogue

---
+++ Assets/_Project/Code/Ship/ShipProfileController.cs
@@ -0,0 +1,63 @@
+using System;
+using Seaborn.Combat;
+using Seaborn.Ship.Data;
+using UnityEngine;
+
+namespace Seaborn.Ship
+{
+    [DisallowMultipleComponent]
+    [DefaultExecutionOrder(-100)]
+    public sealed class ShipProfileController : MonoBehaviour
+    {
+        [SerializeField] private string shipId = "starter_sloop";
+        [SerializeField] private bool restoreHealthWhenApplied = true;
+
+        public event Action<ShipDefinition> ProfileApplied;
+
+        public string ShipId => shipId;
+        public ShipDefinition Definition { get; private set; }
+
+        private void Awake()
+        {
+            ApplyProfile();
+        }
+
+        [ContextMenu("Apply Ship Profile")]
+        public void ApplyProfile()
+        {
+            if (!ShipCatalog.TryGet(shipId, out ShipDefinition definition))
+            {
+                Debug.LogError($"Unknown ship profile: {shipId}", this);
+                return;
+            }
+
+            Definition = definition;
+
+            ShipHealth health = GetComponentInChildren<ShipHealth>();
+            health?.SetBaseMaximumHealth(
+                definition.maximumHealth,
+                restoreHealthWhenApplied
+            );
+
+            BroadsideController broadside =
+                GetComponentInChildren<BroadsideController>();
+            broadside?.SetShipConfiguration(
+                definition.cannonSlots,
+                definition.startingCannons,
+                definition.cannonRange
+            );
+
+            ProfileApplied?.Invoke(definition);
+        }
+
+        public bool TrySelectProfile(string nextShipId, bool restoreHealth)
+        {
+            if (!ShipCatalog.TryGet(nextShipId, out _)) return false;
+
+            shipId = nextShipId;
+            restoreHealthWhenApplied = restoreHealth;
+            ApplyProfile();
+            return Definition != null;
+        }
+    }
+}
