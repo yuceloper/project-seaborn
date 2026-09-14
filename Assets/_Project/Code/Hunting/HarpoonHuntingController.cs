7de5d7467fce21b1adf354c85eb866e2642db87a
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-09-14 22:32:08 +0300

    feat(progression): bootstrap deck extension inventory

--- Assets/_Project/Code/Hunting/HarpoonHuntingController.cs
+++ Assets/_Project/Code/Hunting/HarpoonHuntingController.cs
@@ -530,6 +530,9 @@ namespace Seaborn.Hunting
             Seaborn.Progression
                 .PrototypeFleetInventory
                 .EnsureAttached(player.transform);
+            Seaborn.Progression
+                .PrototypeDeckExtensionInventory
+                .EnsureAttached(player.transform);
 
             if (isHarbor)
             {
