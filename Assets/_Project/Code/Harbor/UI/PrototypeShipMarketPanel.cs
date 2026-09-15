e50cfc2c9fcadb38a6e052c1c37e6588cd81e660
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-09-15 10:52:20 +0300

    style(ui): center focused harbor panels

--- Assets/_Project/Code/Harbor/UI/PrototypeShipMarketPanel.cs
+++ Assets/_Project/Code/Harbor/UI/PrototypeShipMarketPanel.cs
@@ -117,11 +117,11 @@ namespace Seaborn.Harbor.UI
             panel.transform.SetParent(transform, false);
             RectTransform rect =
                 panel.GetComponent<RectTransform>();
-            rect.anchorMin = new Vector2(1f, 0f);
-            rect.anchorMax = new Vector2(1f, 0f);
-            rect.pivot = new Vector2(1f, 0f);
+            rect.anchorMin = new Vector2(0.5f, 0.5f);
+            rect.anchorMax = new Vector2(0.5f, 0.5f);
+            rect.pivot = new Vector2(0.5f, 0.5f);
             rect.anchoredPosition =
-                new Vector2(-936f, 24f);
+                new Vector2(0f, -48f);
             rect.sizeDelta = new Vector2(450f, 462f);
             panel.GetComponent<Image>().color = Navy;
 
