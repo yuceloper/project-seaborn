157e2b5f40a9cf606f7a39cf4c030deab08f66e9
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-09-15 10:53:14 +0300

    style(ui): center focused harbor panels

--- Assets/_Project/Code/Harbor/UI/PrototypeHarborContractPanel.cs
+++ Assets/_Project/Code/Harbor/UI/PrototypeHarborContractPanel.cs
@@ -152,11 +152,11 @@ namespace Seaborn.Harbor.UI
             );
             RectTransform rect =
                 panel.GetComponent<RectTransform>();
-            rect.anchorMin = new Vector2(1f, 1f);
-            rect.anchorMax = new Vector2(1f, 1f);
-            rect.pivot = new Vector2(1f, 1f);
+            rect.anchorMin = new Vector2(0.5f, 0.5f);
+            rect.anchorMax = new Vector2(0.5f, 0.5f);
+            rect.pivot = new Vector2(0.5f, 0.5f);
             rect.anchoredPosition =
-                new Vector2(-24f, -24f);
+                new Vector2(0f, -48f);
             rect.sizeDelta = new Vector2(500f, 680f);
 
             GameObject accent = CreateBlock(
